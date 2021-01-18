5a0b428438f52056b4c690fb4efb91b0658056c6
mca <mca@bec.dk>
2021-01-18 10:51:41 +0100

    named streaming topics counts

--- kafka-streams-for-data-processing/src/main/java/com/github/learnkafka/streams/WordCountTopologyBuilder.java
+++ kafka-streams-for-data-processing/src/main/java/com/github/learnkafka/streams/WordCountTopologyBuilder.java
@@ -1,10 +1,12 @@
 package com.github.learnkafka.streams;
 
+import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.Materialized;
 import org.apache.kafka.streams.kstream.Produced;
 
 import java.util.Arrays;
@@ -14,10 +16,12 @@ public class WordCountTopologyBuilder {
     public static Topology buildWordCountTopology(String inputTopic, String outputTopic) {
         StreamsBuilder streamsBuilder = new StreamsBuilder();
         KStream<String, String> textLines = streamsBuilder.stream(inputTopic);
+        Serde<String> keySerde = Serdes.String();
+        Serde<Long> valueSerde = Serdes.Long();
         KTable<String, Long> wordCounts = textLines.flatMapValues(WordCountTopologyBuilder::splitOnWhiteSpace)
                 .groupBy((key, word) -> word)
-                .count();
-        wordCounts.toStream().to(outputTopic, Produced.with(Serdes.String(), Serdes.Long()));
+                .count(Materialized.as("Counts"));
+        wordCounts.toStream().to(outputTopic, Produced.with(keySerde, valueSerde));
 
         return streamsBuilder.build();
     }
